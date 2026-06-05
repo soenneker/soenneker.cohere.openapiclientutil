64f7b18e959cdd0f6760280e88ff95f1e90935d9
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:05 -0500

    xml commenting

--- src/Soenneker.Cohere.OpenApiClientUtil/Abstract/ICohereOpenApiClientUtil.cs
+++ src/Soenneker.Cohere.OpenApiClientUtil/Abstract/ICohereOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Cohere.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface ICohereOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<CohereOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
